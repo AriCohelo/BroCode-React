7b2f25439e58c9a14ccf5961341a15cf3b6267d6
AriCohelo <ariel.cohelo@gmail.com>
2024-05-08 00:19:13 +0200

    first exercise video

---
+++ src/header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+	return (
+		<header>
+			<h1>My React</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="#"></a>home
+					</li>
+					<li>
+						<a href="#"></a>about
+					</li>
+					<li>
+						<a href="#"></a>services
+					</li>
+					<li>
+						<a href="#"></a>contact
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+}
+export default Header;
